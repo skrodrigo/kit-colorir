b7310d67b6048cd467a882fff2e8e4ef39b333e8
skrodrigo <rodrigoa0987@gmail.com>
2025-07-27 22:39:03 -0300

    feat: add pixel facebook

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Kit de Atividades Criativas",
@@ -22,6 +24,33 @@ export default function RootLayout({
       <body
         className="antialiased winky-rough"
       >
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '630915322869593');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <Image
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=630915322869593&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
       </body>
     </html>
